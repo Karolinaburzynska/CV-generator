b3772f0839882b58cd98fcf33948645c22a32717
kburz <kburz@kursGIT.pl>
2023-01-29 11:23:32 +0100

    Initial commit

---
+++ src/main/java/com/example/projectcv/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.projectcv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getPerson (){
+        return "/home";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
