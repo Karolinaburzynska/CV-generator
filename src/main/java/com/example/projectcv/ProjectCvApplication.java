50d21cc0b2fac4ea543edfaec0d4225bc277113f
kburz <kburz@kursGIT.pl>
2023-01-29 11:52:24 +0100

    initial commit

---
+++ src/main/java/com/example/projectcv/ProjectCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCvApplication.class, args);
+    }
+
+}
